aa70cb0a3bb80d7da768eac747942d78f3a1781c
eleander56 <95469786+eleander56-b@users.noreply.github.com>
2023-01-11 02:51:38 +0100

    Fix für die Spotify Einbindung

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 dayjs.extend(window.dayjs_plugin_duration);
 dayjs.extend(window.dayjs_plugin_customParseFormat);
+function toSeconds(time) {
+  var parts = time.split(':');
+  return (+parts[0]) * 60 * 60 + (+parts[1]) * 60 + (+parts[2]);
+}
+
 
 document.addEventListener("DOMContentLoaded", () => {
   let host = "https://data.hobbylos.online/graphql";
@@ -19,7 +24,8 @@ document.addEventListener("DOMContentLoaded", () => {
       let insertData = data.data.mathefacts.map(({thema, beschreibung, startzeit, endzeit, folge}) => {
         let [start, end] = [startzeit, endzeit].map(el => dayjs(el, "hh:mm:ss"));
         let diff = dayjs.duration(end.diff(start));
-        let embed = "<iframe style=\"border-radius:12px\" src=\"https://open.spotify.com/embed/episode/" + folge.code + "?t=" + dayjs(startzeit, "hh:mm:ss").diff(dayjs("00:00:00", "hh:mm:ss")) + "\" width=\"330\" height=\"152 \" frameBorder=\"0\" allowfullscreen=\"\" allow=\"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\"></iframe>"
+        console.log(dayjs(startzeit, "HH:mm:ss"))
+        let embed = "<iframe style=\"border-radius:12px\" src=\"https://open.spotify.com/embed/episode/" + folge.code + "?t=" + toSeconds(startzeit) + "\" width=\"330\" height=\"152 \" frameBorder=\"0\" allowfullscreen=\"\" allow=\"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture\"></iframe>"
         return [folge.folgenId, folge.folgenname, thema, beschreibung, startzeit, endzeit, diff.format("mm:ss"), embed];
       });
       table.rows().add(insertData);
